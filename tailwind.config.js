40ac65cb1c75f9910a09c047739d8720366fa4a7
Dimas Riatmodjo <dimasriat@gmail.com>
2022-06-06 21:32:06 +0700

    ✦ Fix tailwind config bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     "src/pages/**/*.{js,ts,jsx,tsx}",
     "src/components/**/*.{js,ts,jsx,tsx}",
+    "src/modules/**/*.{js,ts,jsx,tsx}",
+    "src/hooks/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
